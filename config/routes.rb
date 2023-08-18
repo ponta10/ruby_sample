30fe76abac4e7f2a4e83d6be5f23912e595dc626
ponta10 <mponta2002@gmail.com>
2023-08-18 17:33:50 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    # Create
+    get 'articles/new', to: 'articles#new', as: 'new_article' # 新規作成フォーム表示
+    post 'articles', to: 'articles#create'                    # 新規作成
+  
+    # Read
+    get 'articles', to: 'articles#index', as: 'articles'      # 一覧表示
+    get 'articles/:id', to: 'articles#show', as: 'article'    # 詳細表示
+  
+    # Update
+    get 'articles/:id/edit', to: 'articles#edit', as: 'edit_article' # 編集フォーム表示
+    patch 'articles/:id', to: 'articles#update'                        # 更新
+  
+    # Delete
+    delete 'articles/:id', to: 'articles#destroy'                     # 削除
+end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+scope '/admin' do
+  get 'users', to: 'users#index'
 end
